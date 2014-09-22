d8c485226b45a71324bfab6c628e1a92f2a5d2ca
Pitsanu-T <pitsanu.t@carpass.co.th>
2014-09-22 14:36:44 +0700

    add chapter 1

---
+++ Learning WCF book/Chapter1/ServiceFromScratch/Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EndpointAddress ep = new EndpointAddress("http://localhost:8000/HelloIndigo/HelloIndigoService");
+            IHelloIndigoService proxy = ChannelFactory<IHelloIndigoService>.CreateChannel(new BasicHttpBinding(), ep);
+            string s = proxy.HelloIndigo();
+
+            System.Console.WriteLine(s);
+            System.Console.WriteLine("Press <ENTER> to terminate Client.");
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ Learning WCF book/Chapter1/ServiceFromScratch/HelloIndigo/Service.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace HelloIndigo
+{
+    [ServiceContract(Namespace = "my.helloindigo.service.v1")]
+    public interface IHelloIndigoService
+    {
+        [OperationContract]
+        string HelloIndigo();
+    }
+
+    public class HelloIndigoService : IHelloIndigoService
+    {
+        public string HelloIndigo()
+        {
+            return "Hello Indigo";
+        }
+    }
+}
